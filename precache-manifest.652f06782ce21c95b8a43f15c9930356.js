dd2e4f74a9ddf3708a0ce54d4a6fae0afdbf1949
saurabh_singh <imsaurabhsingh007@gmail.com>
2020-05-26 15:33:44 +0530

    Updates

---
+++ precache-manifest.652f06782ce21c95b8a43f15c9930356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02488c31586b3027d69d5d584617ca96",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "52c023fddfdb8b4d93a7",
+    "url": "/monster-rolodex/static/css/main.f770e417.chunk.css"
+  },
+  {
+    "revision": "cf79ba3b4c347c229c69",
+    "url": "/monster-rolodex/static/js/2.e52f72a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.e52f72a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52c023fddfdb8b4d93a7",
+    "url": "/monster-rolodex/static/js/main.4c8ebe09.chunk.js"
+  },
+  {
+    "revision": "753b30f80137f55e537c",
+    "url": "/monster-rolodex/static/js/runtime-main.e7a3ea5b.js"
+  }
+]);
